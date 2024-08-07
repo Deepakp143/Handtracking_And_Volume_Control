ab1c431506973729cef408546e2017d0a39fe4f1
Deepak Prajapat <2021pietaddeepak015@poornima.org>
2024-08-07 22:29:14 +0530

    1 commit

---
+++ HandTrackingModule.py
@@ -0,0 +1,87 @@
+import cv2
+import mediapipe as mp
+import time
+
+class handDetector:
+    def __init__(self, mode=False, maxHands=2, modelC=1, detectionCon=0.5, trackCon=0.5):
+        self.mode = mode
+        self.maxHands = maxHands
+        self.modelC = modelC
+        self.detectionCon = detectionCon
+        self.trackCon = trackCon
+
+        self.mpHands = mp.solutions.hands
+        self.hands = self.mpHands.Hands(self.mode, self.maxHands, self.modelC, self.detectionCon, self.trackCon)
+        self.mpDraw = mp.solutions.drawing_utils
+
+    def findHands(self, img, draw=True):
+        imgRGB = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+        self.results = self.hands.process(imgRGB)
+
+        if self.results.multi_hand_landmarks:
+            for handLms in self.results.multi_hand_landmarks:
+                if draw:
+                    self.mpDraw.draw_landmarks(img, handLms, self.mpHands.HAND_CONNECTIONS)
+
+        return img
+
+    def findPosition(self, img, handNo=0, draw=True):
+        xList = []
+        yList = []
+        bbox = []
+        self.lmList = []
+        if self.results.multi_hand_landmarks:
+            myHand = self.results.multi_hand_landmarks[handNo]
+            for id, lm in enumerate(myHand.landmark):
+                h, w, c = img.shape
+                cx, cy = int(lm.x * w), int(lm.y * h)
+                xList.append(cx)
+                yList.append(cy)
+                self.lmList.append([id, cx, cy])
+                if draw:
+                    cv2.circle(img, (cx, cy), 5, (255, 0, 255), cv2.FILLED)
+
+            xmin, xmax = min(xList), max(xList)
+            ymin, ymax = min(yList), max(yList)
+            bbox = xmin, ymin, xmax, ymax
+
+            if draw:
+                cv2.rectangle(img, (xmin - 20, ymin - 20), (xmax + 20, ymax + 20),
+                              (0, 255, 0), 2)
+
+        return self.lmList, bbox
+
+def main():
+    c_time = 0
+    p_time = 0
+    cam = cv2.VideoCapture(0)
+    
+    if not cam.isOpened():
+        print("Error: Camera could not be opened.")
+        return
+
+    detector = handDetector()
+    while True:
+        success, img = cam.read()
+        if not success:
+            print("Failed to capture image from camera.")
+            break
+
+        img = detector.findHands(img)
+
+        c_time = time.time()
+        fps = 1 / (c_time - p_time)
+        p_time = c_time
+        cv2.putText(img, str(int(fps)), (10, 70), cv2.FONT_HERSHEY_PLAIN, 3, (255, 0, 255), 3)
+
+        cv2.imshow("Image", img)
+        key = cv2.waitKey(1)
+        if key == ord('s'):
+            break
+        elif key == 27:  # ESC key
+            break
+    cam.release()
+    cv2.destroyAllWindows()
+
+if __name__ == "__main__":
+    main()
